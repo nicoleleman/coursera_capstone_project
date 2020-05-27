facd5c0ed030d0d36ea96e803c75536b7e9baec3
nicoleleman <nicoman329@outlook.com>
2020-05-27 10:35:15 +0100

    Updated project notebook, cleared cell outputs to reduce file size

--- src/data/street_lat_lon.py
+++ src/data/street_lat_lon.py
@@ -9,20 +9,28 @@ from geopy.geocoders import Nominatim
 import requests
 from pandas.io.json import json_normalize
 
+# Read in the dataframe and assign column headers
 ppd_2019 = pd.read_csv('../../data/external/pp-2019.csv')
 ppd_2019.columns = ['TUID', 'Price', 'Date_of_Transfer', 'Postcode', 'Property_Type', 'Old_New', 'Duration',
                     'PAON', 'SAON', 'Street', 'Locality', 'Town_City', 'District', 'County', 'PPD_Cat_Type', 'Record_Status']
 ppd_2019.sort_values(by=['Date_of_Transfer'], ascending=False, inplace=True)
 
+# Drop features that are irrelevant for this project, filter for London rows and clean up the data
 ppd_2019_clean = ppd_2019.drop(columns=['TUID', 'Duration', 'PAON', 'SAON', 'Locality', 'PPD_Cat_Type', 'Record_Status'])
+ppd_london = ppd_2019_clean[ppd_2019['Town_City']=='LONDON'].copy()
+ppd_london = ppd_london.drop(ppd_london[ppd_london.Price > 2000000].index)
+ppd_london.dropna(axis=0, how='any', inplace=True)
 
-ppd_london = ppd_2019_clean[ppd_2019['Town_City']=='LONDON']
+# Add a new column for the postcode prefixes
+ppd_london['Postcode_Prefix'] = ppd_london['Postcode'].apply(lambda x: x.split(' ')[0])
 
-ppd_grouped = ppd_london.groupby(['Street'])['Price'].mean().round(0).reset_index()
-ppd_grouped.columns = ['street', 'avg_price']
+# Group bt the street name and postcode pre fixes
+ppd_grouped = ppd_london.groupby(['Street', 'District', 'Postcode_Prefix'])['Price'].mean().round(0).reset_index()
+ppd_grouped.columns = ['street', 'district', 'postcode_prefix', 'avg_price']
+#print(ppd_grouped)
 
 with open ('../../data/processed/ppd_london_2019.csv', 'w', encoding='utf-8', newline='') as f:
-    column_headers = ['street', 'avg_price', 'latitude', 'longitude']
+    column_headers = ['street', 'district', 'postcode_prefix', 'avg_price', 'latitude', 'longitude']
     writer = csv.DictWriter(f, fieldnames=column_headers)
     writer.writeheader()
     i = 0
@@ -30,15 +38,17 @@ with open ('../../data/processed/ppd_london_2019.csv', 'w', encoding='utf-8', ne
 
     for street_name in ppd_grouped['street']:
         avg_price = ppd_grouped['avg_price'][i]
+        district = ppd_grouped['district'][i]
+        postcode_prefix = ppd_grouped['postcode_prefix'][i]
         i += 1
         try:
             # LONDON is appended to the street name as in some cases the incorrect lat and lon were pulled
-            latitude = geolocator.geocode(street_name+', LONDON').latitude
-            longitude = geolocator.geocode(street_name+', LONDON').longitude
+            latitude = geolocator.geocode(street_name + ', ' + postcode_prefix).latitude
+            longitude = geolocator.geocode(street_name + ', ' + postcode_prefix).longitude
             print(i)
 
-            writer.writerow({'street': street_name, 'avg_price': avg_price,
-                             'latitude': latitude, 'longitude': longitude})
+            writer.writerow({'street': street_name, 'postcode_prefix': postcode_prefix, 'district': district,
+                             'avg_price': avg_price, 'latitude': latitude, 'longitude': longitude})
         except Exception as e:
-            writer.writerow({'street': street_name, 'avg_price': avg_price,
-                             'latitude': 'Nan', 'longitude': 'Nan'})
+            writer.writerow({'street': street_name, 'postcode_prefix': postcode_prefix, 'district': district,
+                             'avg_price': avg_price, 'latitude': 'NaN', 'longitude': 'NaN'})
